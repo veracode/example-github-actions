1525046edbfd0c25bd6774486a6bddebfcf5b9f0
Darius Foo <darius@srcclr.com>
2019-02-26 16:35:46 +0800

    Update Advisor demo

--- src/main/java/com/srcclr/Main.java
+++ src/main/java/com/srcclr/Main.java
@@ -3,18 +3,22 @@ package com.srcclr;
 import org.apache.commons.fileupload.MultipartStream;
 import org.apache.xml.security.signature.XMLSignatureInput;
 import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.web.util.UriUtils;
 
 import java.io.ByteArrayInputStream;
 
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     String candidate = args[0];
     String hashed = BCrypt.hashpw(candidate, BCrypt.gensalt(12));
 
     BCrypt.checkpw(candidate, hashed);
 
     filterXMLSignature();
+
+    // Update Advisor: changed in the upgrade from Spring Web 3.1.1.RELEASE to 3.2.15.RELEASE
+    UriUtils.encodeFragment("", "");
   }
 
   private static void filterXMLSignature() {
